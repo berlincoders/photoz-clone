33e6df840cf28464fdedc1aacd7aa78c48749a8c
Romeo <ralonsoad@gmail.com>
2024-04-06 23:21:38 +0200

    throw an error 404

--- src/main/java/com/berlincoders/romeo/photoz/clone/PhotozController.java
+++ src/main/java/com/berlincoders/romeo/photoz/clone/PhotozController.java
@@ -1,15 +1,25 @@
 package com.berlincoders.romeo.photoz.clone;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 
 public class PhotozController {
 
-    private List<Photo> db = List.of(new Photo("1","hello.jpg"));
+    // what is a map in Java?
+    private Map<String, Photo> db = new HashMap<>() {{
+        put("1", new Photo("1","hello.jpg"));
+    }};
 
     @GetMapping("/")
     public String hello() {
@@ -17,8 +27,14 @@ public class PhotozController {
     }
     //Adding endpoint
     @GetMapping ("/photoz")
-    public List<Photo>  get(){
+    public Collection<Photo> get(){
+     return db.values();
+    }
 
-     return db;
+    @GetMapping ("/photoz/{id}")
+    public Photo  get(@PathVariable String id){
+        Photo photo = db.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
     }
 }
