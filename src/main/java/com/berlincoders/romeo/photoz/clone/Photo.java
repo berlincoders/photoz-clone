a13b687bd52b9f21f028a2d11db25d3606069a04
Romeo <ralonsoad@gmail.com>
2024-04-06 19:59:32 +0200

    Add photo class Model

---
+++ src/main/java/com/berlincoders/romeo/photoz/clone/Photo.java
@@ -0,0 +1,26 @@
+package com.berlincoders.romeo.photoz.clone;
+
+public class Photo {
+
+    private  String id:
+
+    private String FileName;
+
+    //raw data
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return FileName;
+    }
+
+    public void setFileName(String fileName) {
+        FileName = fileName;
+    }
+}
